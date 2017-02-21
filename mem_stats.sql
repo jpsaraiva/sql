2872a71b99e36db8c8bf08a60ddac78c4e5cc47b
jpsaraiva <j3.saraiva@gmail.com>
2017-02-21 12:19:20 +0000

    v.3

--- mem_stats.sql
+++ mem_stats.sql
@@ -19,9 +19,10 @@ set pagesize 100 lines 120 pages 1000 heading on feed off null '' ver off
 
 col "INST" for 9
 col sql_id for a13
-col "SCHEMA" for a15
+col "SCHEMA" for a13
 col "ELAPSED_TIME" for 999999,99
 col "CPU_TIME" for 999999,99
+col "ROWS" for 999999
 
 select 
     distinct inst_id "INST",
@@ -30,7 +31,7 @@ select
     parsing_schema_name "SCHEMA",
     executions,
     to_char(last_active_time,'YYYY-MM-DD HH24:MI:SS') "DATA",
-    rows_processed, 
+    rows_processed "ROWS", 
     round(elapsed_time/1000000/decode(executions,0,1,executions),2) "ELAPSED_TIME",
     round(cpu_time/1000000/decode(executions,0,1,executions),2) "CPU_TIME"
 from gv$sql where sql_id = '&1';
---
+++ sess_events.sql
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------------
+--
+-- File name: activ.sql
+--
+-- Purpose: Lists active sessions on RAC database
+--
+-- Author: jpsaraiva
+--
+-- Version: 2017/02/13
+--
+-- Example: @activ.sql
+--
+-- Notes: 	Developed and tested on 11.2.0.4.
+--			RAC enabled
+--			This script	excludes slaves from view
+--
+---------------------------------------------------------------------------------------
+--
+
+set pagesize 100 lines 120 pages 1000 heading on feed off null '' ver off
+
+column sse        format a15
+column status     format a10
+column username   format a13
+column status     format a10
+column source     format a20
+column logon_time format a20
+column event		format a25 word_wrapped
+column "ELAPSED TIME"  format a12
+column sql_id    format a14
+
+select sid||','||serial#||'@'||inst_id||'' sse
+     , username
+     , to_char(logon_time, 'YYYY-MM-DD HH24:MI:SS') logon_time
+     , status
+     , sql_id
+	 , event
+     ,  lpad(trunc(LAST_CALL_ET/86400),2,0)||' '||
+        lpad(trunc((LAST_CALL_ET/86400-trunc(LAST_CALL_ET/86400))*24),2,0)||':'||
+        lpad (trunc((LAST_CALL_ET/3600-trunc(LAST_CALL_ET/3600))*60),2,0)||':'||
+        lpad(trunc(((LAST_CALL_ET/3600-trunc(LAST_CALL_ET/3600))*60-trunc((LAST_CALL_ET/3600-trunc(LAST_CALL_ET/3600))*60))*60),2,0)"ELAPSED TIME"
+ from gv$session
+where sid=&1
+order by 7;
+
